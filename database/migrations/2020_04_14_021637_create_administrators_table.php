e8ae18e014ef26bf31ca7bab0b96885be5e06b5a
imJull <julljidw@gmail.com>
2020-04-13 20:57:12 -0600

    Migrations

---
+++ database/migrations/2020_04_14_021637_create_administrators_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdministratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrators', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('CodigoAdmin');
+            $table->string('Nombre');
+            $table->string('Contraseña');
+            $table->string('Departamento');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrators');
+    }
+}
---
+++ database/migrations/2020_04_14_021703_create_hours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hours', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('HoraId');
+            $table->date('Fecha');
+            $table->integer('HoraEntrada');
+            $table->integer('HoraSalida');
+            $table->string('Descripcion');
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hours');
+    }
+}
